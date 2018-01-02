90465234d69a32a14ca82ffdd3cf28384d3bfe3a
zhoubi <32322505@qq.com>
2018-01-02 17:00:30 +0800

    init

---
+++ src/test/java/com/eureka/zuul/eurekazuul/EurekazuulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eureka.zuul.eurekazuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekazuulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
